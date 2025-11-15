46b3cebb2e1cf3a545d2fd9e7fea6611e9fd0224
mamaofthree579-ship-it <mamaofthree579@gmail.com>
2025-11-14 21:21:47 -0500

    Update grid.py

--- simulator/grid.py
+++ simulator/grid.py
@@ -1,7 +1,10 @@
 import numpy as np
 
-def create_grid(grid_size=256):
-    X = np.linspace(0, 1, grid_size)
-    Y = np.linspace(0, 1, grid_size)
+def create_grid(grid_size=200):
+    """
+    Small convenience wrapper: returns x,y meshgrid in normalized coords 0..1
+    """
+    X = np.linspace(0.0, 1.0, grid_size)
+    Y = np.linspace(0.0, 1.0, grid_size)
     XX, YY = np.meshgrid(X, Y)
     return XX, YY
